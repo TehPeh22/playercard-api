ab931179808006a738cc3900583ad3fed7c25690
TehPeh22 <tpeh22@gmail.com>
2025-09-29 01:12:35 -0400

    implemented navbar

--- src/Components/Navbar/Nabbar.jsx
+++ src/Components/Navbar/Nabbar.jsx
@@ -3,7 +3,19 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div className='navbar'>
-            
+            <div className='logo'>
+                <img src='#'></img>
+            </div>
+            <div className='search-bar'>
+                <input type='text' placeholder='search...'></input>
+            </div>
+            <ul className='nav-menu'>
+                <li>Home</li>
+                <li>Contact</li>
+            </ul>
+            <div className='nav-login'>
+                <button>Login</button>
+            </div>
         </div>
     )
 }
