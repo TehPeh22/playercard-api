c1a92651599a77a0ffc9d1aaf6314d5ee774755d
TehPeh22 <tpeh22@gmail.com>
2025-10-02 20:48:55 -0400

    commit from laptop

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import './LoginSignup.css'
 
 const LoginSignup = () => {
     return (
-        <div>
-            
+        <div className="form-container">
+            <div className="form-content">
+                <div className="form-img">
+                    <img src="#"></img>
+                </div>
+                <div className="form-signin">
+                    <h2>Sign In</h2>
+                    <form>
+                        <label for="firstname">First Name
+                            <input type="text" />
+                        </label>
+                        <label for="lastname">Last Name
+                            <input type="text" />
+                        </label>
+                        <label for="email">Email
+                            <input type="text" />
+                        </label>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
